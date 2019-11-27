9dba9078bec27f8a2d12257f5072238a0adc1d1d
Mirellys Arteta Davila <martetadavila@anwb.nl>
2019-11-27 11:30:03 +0100

    Add first version code

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { AnythingNpm } = NativeModules;
+
+export default AnythingNpm;
---
+++ ios/AnythingNpm.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface AnythingNpm : NSObject <RCTBridgeModule>
+
+@end
